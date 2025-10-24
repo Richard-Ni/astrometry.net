aacab914b3bd4ea17f2f5a12f408db2c93e306b3
nilicheng <starboyseeyou@163.com>
2025-10-24 15:37:14 +0800

    testing

--- config/run_solve_field.sh
+++ config/run_solve_field.sh
@@ -1,16 +1,12 @@
-#!/bin/bash
-# 使用 Source Extractor 配置文件时，必须显式指定列名！
+#!/bin/sh
 
 solve-field \
-  --downsample 4 \
+  --objs 30 \
   --no-remove-lines \
   --uniformize 0 \
   --overwrite \
   --no-plot \
   --fits-image \
-  --scale-low 2 \
-  --scale-high 6 \
-  --scale-units arcsecperpix \
   --use-source-extractor \
   --source-extractor-path sex \
   --source-extractor-config /usr/local/share/sextractor/default.sex \
